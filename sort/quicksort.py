81c9d909c408e8e880b09596e0310558bbb155df
Aiyoj <xushiqitc@163.com>
2018-08-06 21:56:36 +0800

    快速排序

---
+++ sort/quicksort.py
@@ -0,0 +1,30 @@
+def quicksort(arr, left, right):
+    if left > right:
+        return None
+    tmp = arr[left]
+    i = left
+    j = right
+
+    while i != j:
+        while arr[j] >= tmp and i < j:
+            j -= 1
+        while arr[i] <= tmp and i < j:
+            i += 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[left] = arr[i]
+    arr[i] = tmp
+    quicksort(arr, left, i - 1)
+    quicksort(arr, i + 1, right)
+
+
+def main():
+    arr = [3, 5, 2, 6, 1, 4, 9, 6]
+    quicksort(arr, 0, len(arr) - 1)
+    return arr
+
+
+if __name__ == '__main__':
+    result = main()
+    print(result)
