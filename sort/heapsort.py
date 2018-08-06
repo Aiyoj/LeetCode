a5ad95d9d8e594f876c02d84014633cb76ddd00b
Aiyoj <xushiqitc@163.com>
2018-08-06 20:45:44 +0800

    堆排序

---
+++ sort/heapsort.py
@@ -0,0 +1,38 @@
+
+
+def adjustheap(arr, i, length):
+    # 取出当前元素
+    tmp = arr[i]
+    j = 2 * i + 1
+    while j < length:
+        if j + 1 < length and arr[j] < arr[j + 1]:
+            j += 1
+        if arr[j] > tmp:
+            arr[i] = arr[j]
+            i = j
+        else:
+            break
+
+        arr[i] = tmp
+        j = 2 * i + 1
+
+
+def heapsort(arr):
+    length = len(arr)
+    # 构建大顶堆(从下至上，从右至左)
+    for i in range(length // 2 - 1, -1, -1):
+        adjustheap(arr, i, length)
+    for i in range(length-1, -1, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        adjustheap(arr, 0, i)
+
+    print(arr)
+
+
+def main():
+    arr = [9, 8, 75, 8, 3, 7, 3, 2, 1]
+    heapsort(arr)
+
+
+if __name__ == '__main__':
+    main()
